0ec6007d79c58916f5634aa81c38fa6577810cf2
Lucas Tonussi <lptonussi@gmail.com>
2025-02-01 15:42:01 -0300

    Remove stuff

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <main>
+    <RouterView />
+  </main>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <section>
+    <RouterLink to="/privacy-politics">Privacy Politics</RouterLink>
+    <RouterLink to="/service-terms">Service Terms</RouterLink>
+  </section>
 </template>
 
 <style scoped>
