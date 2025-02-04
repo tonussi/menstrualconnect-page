3c6bf502ba886634a0090353014adb76dbf0c166
Lucas Tonussi <lptonussi@gmail.com>
2025-02-04 08:17:22 -0300

    Add download btn

---
+++ src/components/smalls/PlayStore.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const props = defineProps({
+  text: { type: String, required: true },
+  link: { type: String, required: true },
+  type: { type: String, required: true },
+})
+
+const handleClick = (link: string) => {
+  window.open(link, "_blank");
+}
+</script>
+
+<template>
+  <div class="download-store-button-grid" @click="handleClick(link)">
+
+    <img v-if="type == 'PlayStore'" referrerpolicy="no-referrer" class="play-button-pic"
+      src="@/assets/pictures/PlayStore.png" alt="" />
+    <img v-if="type == 'AppleStore'" referrerpolicy="no-referrer" class="play-button-pic"
+      src="@/assets/pictures/AppleStore.png" alt="" />
+
+    <div class="get-on-store">{{ text }}</div>
+
+  </div>
+</template>
+
+<style scoped>
+.download-store-button-grid {
+  width: 250px;
+  border-radius: 15px;
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  display: flex;
+  padding: 15px;
+}
+
+.get-on-store {
+  padding-left: 20px;
+}
+
+.play-button-pic {
+  max-width: 100px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <script setup lang="ts">
+import PlayStore from '@/components/smalls/PlayStore.vue'
 </script>
 
 <template>
-  <div style="max-width: fit-content; margin-left: auto; margin-right: auto">
-    <div style="display: grid; grid-template-columns: max-content; grid-template-rows: 75px 200px">
-      <div style="font-size: 100px; color: var(--color-primary)">Menstrual</div>
-      <div style="font-size: 100px; color: var(--color-secondary); margin-left: 3.99em">
-        Connect
+  <div class="centralize-content">
+
+    <div class="centralize">
+      <div class="main-title-grid">
+        <div style="font-size: 100px; color: var(--color-primary)">Menstrual</div>
+        <div style="font-size: 100px; color: var(--color-secondary); margin-left: 3.45em">
+          Connect
+        </div>
       </div>
     </div>
 
-    <img src="@/assets/pictures/AppCycleSettings.png" alt="AppCycleSettings" />
-    <img src="@/assets/pictures/AppSpousePage.png" alt="AppSpousePage" />
-    <img src="@/assets/pictures/AppCalendarApp.png" alt="AppCalendarApp" />
-    <img src="@/assets/pictures/ExportToCalendar.png" alt="ExportToCalendar" />
+    <div class="centralize" style="margin-bottom: 100px">
+      <PlayStore type="PlayStore" text="Baixar aplicativo Android"
+        link="https://drive.google.com/file/d/1I69DxPLQHeaCKpk50U1s18rI1v83SRhe/view?usp=drive_link"></PlayStore>
+    </div>
+
+    <img referrerpolicy="no-referrer" src="@/assets/pictures/AppCycleSettings.png" alt="AppCycleSettings" />
+    <img referrerpolicy="no-referrer" src="@/assets/pictures/AppSpousePage.png" alt="AppSpousePage" />
+    <img referrerpolicy="no-referrer" src="@/assets/pictures/AppCalendarApp.png" alt="AppCalendarApp" />
+    <img referrerpolicy="no-referrer" src="@/assets/pictures/ExportToCalendar.png" alt="ExportToCalendar" />
+    <img referrerpolicy="no-referrer" src="@/assets/pictures/DarkTheme.png" alt="DarkTheme" />
   </div>
 </template>
+
+<style scoped>
+.centralize {
+  max-width: fit-content;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.centralize-content {
+  max-width: 1000px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.main-title-grid {
+  display: grid;
+  grid-template-columns: max-content;
+  grid-template-rows: 75px 200px
+}
+</style>
