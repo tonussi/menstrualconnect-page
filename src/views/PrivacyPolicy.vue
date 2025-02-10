8f5303d12fce14d8d0105b426dbdee628742d590
Lucas Tonussi <lptonussi@gmail.com>
2025-02-09 21:18:20 -0300

    Update service terms

--- src/views/PrivacyPolicy.vue
+++ src/views/PrivacyPolicy.vue
@@ -3,86 +3,8 @@ import CheckMark from '@/components/icons/CheckMark.vue'
 </script>
 
 <template>
-  <div style="max-width: fit-content; margin-left: auto; margin-right: auto">
-    <div class="text-3xl font-bold">In English:</div>
-    <p class="py-6">
-      <span>Last updated: {{ new Date().toLocaleString() }}</span>
-    </p>
-    <ol>
-      <li class="flex items-center">
-        <check-mark></check-mark>
-        <p class="pl-3">All personal data must be processed in an ethical manner.</p>
-      </li>
-      <li class="flex items-center">
-        <check-mark></check-mark>
-        <p class="pl-3">
-          Data should be collected for predetermined reasons only, and the data must be used for
-          these reasons alone.
-        </p>
-      </li>
-      <li class="flex items-center">
-        <check-mark></check-mark>
-        <p class="pl-3">Data must be accurate and updated when requested.</p>
-      </li>
-      <li class="flex items-center">
-        <check-mark></check-mark>
-        <p class="pl-3">
-          With the exception of specific circumstances, such as scientific research data, the user
-          must be identified only for as long as needed.
-        </p>
-      </li>
-      <li class="flex items-center">
-        <check-mark></check-mark>
-        <p class="pl-3">
-          The business collecting data is responsible for monitoring its own adherence to GDPR
-          regulation through the appointment of a Data Protection Officer, if applicable.
-        </p>
-      </li>
-      <li class="flex items-center">
-        <check-mark></check-mark>
-        <p class="pl-3">
-          The user must be able to contact the business collecting the data and its Data Protection
-          Officer (if there is one).
-        </p>
-      </li>
-      <li class="flex items-center">
-        <check-mark></check-mark>
-        <p class="pl-3">
-          Users must be made aware of the reasons why their data is being gathered and the length of
-          time that it will be stored.
-        </p>
-      </li>
-      <li class="flex items-center">
-        <check-mark></check-mark>
-        <p class="pl-3">
-          Users must be advised of their 8 rights under the GDPR including the right to access,
-          update or request removal of their personal data.
-        </p>
-      </li>
-      <li class="flex items-center">
-        <check-mark></check-mark>
-        <p class="pl-3">
-          There will be a supervisory body to deal with users' complaints and the contact
-          information for this body must be provided.
-        </p>
-      </li>
-      <li class="flex items-center">
-        <check-mark></check-mark>
-        <p class="pl-3">
-          Users must be informed if their data is to be shared with any third parties or affiliated
-          organizations, or if it will be transferred outside of the BRAZIL.
-        </p>
-      </li>
-      <li class="flex items-center">
-        <check-mark></check-mark>
-        <p class="pl-3">
-          Any other information the user needs to know to ensure fair processing of their personal
-          data.
-        </p>
-      </li>
-    </ol>
-
-    <div class="text-3xl font-bold pt-10">In Portuguese:</div>
+  <div class="p-25 justify-start">
+    <div class="text-3xl font-bold">Em português:</div>
     <p class="py-6">
       <span>Última atualização: {{ new Date().toLocaleString() }}</span>
     </p>
@@ -156,6 +78,84 @@ import CheckMark from '@/components/icons/CheckMark.vue'
         <p class="pl-3">
           Qualquer outra informação que o usuário precise saber para garantir o processamento justo
           de seus dados pessoais.
+        </p>
+      </li>
+    </ol>
+
+    <div class="text-3xl font-bold pt-10">In English:</div>
+    <p class="py-6">
+      <span>Last updated: {{ new Date().toLocaleString() }}</span>
+    </p>
+    <ol>
+      <li class="flex items-center">
+        <check-mark></check-mark>
+        <p class="pl-3">All personal data must be processed in an ethical manner.</p>
+      </li>
+      <li class="flex items-center">
+        <check-mark></check-mark>
+        <p class="pl-3">
+          Data should be collected for predetermined reasons only, and the data must be used for
+          these reasons alone.
+        </p>
+      </li>
+      <li class="flex items-center">
+        <check-mark></check-mark>
+        <p class="pl-3">Data must be accurate and updated when requested.</p>
+      </li>
+      <li class="flex items-center">
+        <check-mark></check-mark>
+        <p class="pl-3">
+          With the exception of specific circumstances, such as scientific research data, the user
+          must be identified only for as long as needed.
+        </p>
+      </li>
+      <li class="flex items-center">
+        <check-mark></check-mark>
+        <p class="pl-3">
+          The business collecting data is responsible for monitoring its own adherence to GDPR
+          regulation through the appointment of a Data Protection Officer, if applicable.
+        </p>
+      </li>
+      <li class="flex items-center">
+        <check-mark></check-mark>
+        <p class="pl-3">
+          The user must be able to contact the business collecting the data and its Data Protection
+          Officer (if there is one).
+        </p>
+      </li>
+      <li class="flex items-center">
+        <check-mark></check-mark>
+        <p class="pl-3">
+          Users must be made aware of the reasons why their data is being gathered and the length of
+          time that it will be stored.
+        </p>
+      </li>
+      <li class="flex items-center">
+        <check-mark></check-mark>
+        <p class="pl-3">
+          Users must be advised of their 8 rights under the GDPR including the right to access,
+          update or request removal of their personal data.
+        </p>
+      </li>
+      <li class="flex items-center">
+        <check-mark></check-mark>
+        <p class="pl-3">
+          There will be a supervisory body to deal with users' complaints and the contact
+          information for this body must be provided.
+        </p>
+      </li>
+      <li class="flex items-center">
+        <check-mark></check-mark>
+        <p class="pl-3">
+          Users must be informed if their data is to be shared with any third parties or affiliated
+          organizations, or if it will be transferred outside of the BRAZIL.
+        </p>
+      </li>
+      <li class="flex items-center">
+        <check-mark></check-mark>
+        <p class="pl-3">
+          Any other information the user needs to know to ensure fair processing of their personal
+          data.
         </p>
       </li>
     </ol>
--- src/views/ServiceTerms.vue
+++ src/views/ServiceTerms.vue
@@ -3,90 +3,8 @@ import CheckMark from '@/components/icons/CheckMark.vue'
 </script>
 
 <template>
-  <div style="max-width: fit-content; margin-left: auto; margin-right: auto">
-    <div class="text-3xl font-bold">In English:</div>
-    <p class="font-bold py-5">
-      <span>Terms of Service</span>
-    </p>
-    <p>
-      <span>Last updated: {{ new Date().toLocaleString() }}</span>
-    </p>
-    <p class="font-extralight pt-5">
-      <span
-        >Welcome to Menstrual Connect! By using our app, you agree to the following terms of
-        service. Please read them carefully before proceeding.</span
-      >
-    </p>
-    <div class="text-xl font-bold py-5"><span>Using the App</span></div>
-    <p class="flex items-center">
-      <check-mark></check-mark>
-      <span class="pl-3"
-        >Menstrual Connect allows you to create and manage events directly in your Google calendar.
-        To do this, you need to grant access permissions to your calendar.</span
-      >
-    </p>
-    <div class="text-xl font-bold py-5"><span>Data Collection and Use</span></div>
-    <p class="flex items-center">
-      <check-mark></check-mark>
-      <span class="pl-3"
-        >Our application only accesses and stores the information necessary to create, modify, and
-        manage events in your calendar. We do not share your data with third parties without your
-        consent.</span
-      >
-    </p>
-    <div class="text-xl font-bold py-5"><span>User Responsibilities</span></div>
-    <ul>
-      <li>
-        <p class="flex items-center">
-          <check-mark></check-mark>
-          <span class="pl-3">Ensure that you have permission to grant access to the calendar;</span>
-        </p>
-      </li>
-      <li>
-        <p class="flex items-center">
-          <check-mark></check-mark>
-          <span class="pl-3"
-            >Use the application ethically and in compliance with applicable laws;</span
-          >
-        </p>
-      </li>
-      <li>
-        <p class="flex items-center">
-          <check-mark></check-mark>
-          <span class="pl-3">Do not use the service for malicious or fraudulent activities.</span>
-        </p>
-      </li>
-    </ul>
-    <div class="text-xl font-bold py-5"><span>Limitation of Liability</span></div>
-    <p class="flex items-center">
-      <check-mark></check-mark>
-      <span class="pl-3">
-        >We are not responsible for any loss of data, sync failures or any damages arising from the
-        use of the application. It is the user's responsibility to verify the information
-        entered.</span
-      >
-    </p>
-    <div class="text-xl font-bold py-5"><span>Modifications to the Terms</span></div>
-    <p class="flex items-center">
-      <check-mark></check-mark>
-      <span class="pl-3">
-        We may update these terms from time to time. Your continued use of the application after
-        changes constitutes your acceptance of the new terms.</span
-      >
-    </p>
-    <div class="text-xl font-bold py-5"><span>Contact</span></div>
-    <p class="flex items-center">
-      <check-mark></check-mark>
-      <span class="pl-3">
-        If you have any questions or need support, please contact us at: lptonussi@gmail.com.</span
-      >
-    </p>
-    <p class="flex items-center">
-      <check-mark></check-mark>
-      <span class="pl-3">By using Menstrual Connect, you agree to these Terms of Service.</span>
-    </p>
-
-    <div class="text-3xl font-bold pt-10">In Portuguese:</div>
+  <div class="p-25 justify-start">
+    <div class="text-3xl font-bold">Em português:</div>
     <p class="font-bold py-5">
       <span>Termos de Serviço</span>
     </p>
@@ -147,9 +65,14 @@ import CheckMark from '@/components/icons/CheckMark.vue'
     <p class="flex items-center">
       <check-mark></check-mark>
       <span class="pl-3"
-        >Não nos responsabilizamos por perdas de dados, falhas de sincronização ou qualquer dano
-        decorrente do uso do aplicativo. O usuário é responsável por verificar as informações
-        inseridas.</span
+        >Não nos responsabilizamos por perdas de dados dentro do aplicativo, falhas de sincronização
+        ou qualquer dano decorrente do uso do aplicativo. O usuário é responsável por verificar as
+        informações inseridas. O aplicativo apenas cria eventos novos no seu calendário, sendo eles
+        eventos do aplicativo Menstrual Connect. O aplicativo não remove eventos que não sejam do
+        Menstrual Connect. Nossa aplicação se orienta pelo nome do evento, e sendo ele
+        <b>Menstrual Connect</b> no título do evento, esse evento poderá ser removido caso o usuário
+        crie um novo ciclo menstrual. Pois um novo ciclo irá mudar os eventos caso eles forem
+        exportados para o calendário Google.</span
       >
     </p>
     <div class="text-xl font-bold py-5"><span>Modificações nos Termos</span></div>
@@ -164,8 +87,8 @@ import CheckMark from '@/components/icons/CheckMark.vue'
     <p class="flex items-center">
       <check-mark></check-mark>
       <span class="pl-3"
-        >Se tiver dúvidas ou precisar de suporte, entre em contato pelo e-mail: [E-mail de
-        Suporte].</span
+        >Se tiver dúvidas ou precisar de suporte, entre em contato pelo e-mail:
+        lptonussi@gmail.com.</span
       >
     </p>
     <p class="flex items-center">
@@ -173,6 +96,94 @@ import CheckMark from '@/components/icons/CheckMark.vue'
       <span class="pl-3"
         >Ao utilizar o Menstrual Connect, você concorda com estes Termos de Serviço.</span
       >
+    </p>
+
+    <div class="text-3xl font-bold pt-10">In English:</div>
+    <p class="font-bold py-5">
+      <span>Terms of Service</span>
+    </p>
+    <p>
+      <span>Last updated: {{ new Date().toLocaleString() }}</span>
+    </p>
+    <p class="font-extralight pt-5">
+      <span
+        >Welcome to Menstrual Connect! By using our app, you agree to the following terms of
+        service. Please read them carefully before proceeding.</span
+      >
+    </p>
+    <div class="text-xl font-bold py-5"><span>Using the App</span></div>
+    <p class="flex items-center">
+      <check-mark></check-mark>
+      <span class="pl-3"
+        >Menstrual Connect allows you to create and manage events directly in your Google calendar.
+        To do this, you need to grant access permissions to your calendar.</span
+      >
+    </p>
+    <div class="text-xl font-bold py-5"><span>Data Collection and Use</span></div>
+    <p class="flex items-center">
+      <check-mark></check-mark>
+      <span class="pl-3"
+        >Our application only accesses and stores the information necessary to create, modify, and
+        manage events in your calendar. We do not share your data with third parties without your
+        consent.</span
+      >
+    </p>
+    <div class="text-xl font-bold py-5"><span>User Responsibilities</span></div>
+    <ul>
+      <li>
+        <p class="flex items-center">
+          <check-mark></check-mark>
+          <span class="pl-3">Ensure that you have permission to grant access to the calendar;</span>
+        </p>
+      </li>
+      <li>
+        <p class="flex items-center">
+          <check-mark></check-mark>
+          <span class="pl-3"
+            >Use the application ethically and in compliance with applicable laws;</span
+          >
+        </p>
+      </li>
+      <li>
+        <p class="flex items-center">
+          <check-mark></check-mark>
+          <span class="pl-3">Do not use the service for malicious or fraudulent activities.</span>
+        </p>
+      </li>
+    </ul>
+    <div class="text-xl font-bold py-5"><span>Limitation of Liability</span></div>
+    <p class="flex items-center">
+      <check-mark></check-mark>
+      <span class="pl-3">
+        We are not responsible for data losses within the application, synchronization failures or
+        any damage resulting from the use of the application. The user is responsible for verifying
+        the inserted information. The application only creates new events of Menstrual Connect
+        (title) on your Google Calendar, in other words they are Menstrual Connect's events. The
+        application does not remove events other than Menstrual Connect's. Our application is
+        oriented by the name of the event, this means the title of the event will have Menstrual
+        Connect. Those events with title Menstrual Connect can be removed if the user setup a new
+        menstrual cycle, because a new cycle will change the events if they are exported to the
+        Google calendar.
+      </span>
+    </p>
+    <div class="text-xl font-bold py-5"><span>Modifications to the Terms</span></div>
+    <p class="flex items-center">
+      <check-mark></check-mark>
+      <span class="pl-3">
+        We may update these terms from time to time. Your continued use of the application after
+        changes constitutes your acceptance of the new terms.</span
+      >
+    </p>
+    <div class="text-xl font-bold py-5"><span>Contact</span></div>
+    <p class="flex items-center">
+      <check-mark></check-mark>
+      <span class="pl-3">
+        If you have any questions or need support, please contact us at: lptonussi@gmail.com.</span
+      >
+    </p>
+    <p class="flex items-center">
+      <check-mark></check-mark>
+      <span class="pl-3">By using Menstrual Connect, you agree to these Terms of Service.</span>
     </p>
   </div>
 </template>
