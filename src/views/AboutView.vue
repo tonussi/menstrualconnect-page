bf8a614ce956b3048eb5fe737fc012f132cf228b
Lucas Tonussi <lptonussi@gmail.com>
2025-06-19 11:48:39 -0300

    fix: Improve wording in AboutView description for clarity

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,9 +2,9 @@
 
 <template>
   <div class="p-25 justify-start h-screen">
-    O Menstrual Connect é um aplicativo para ajudar as mulheres a monitorar seu ciclo, porém com um
-    toque a mais de planejamento. O Menstrual Connect se integra ao seu Calendário Google, para
-    lançar lembretes ao casal. A ideia é que o marido também saiba das necessidades da mulher, possa
-    ser mais empático e aprender essas necessidades.
+    O Menstrual Connect é um aplicativo para auxiliar, as mulheres, a monitorar seus ciclos, porém
+    com um toque a mais de planejamento. O Menstrual Connect se integra ao seu Calendário Google,
+    para lançar lembretes ao casal. A ideia é que o marido também saiba das necessidades da mulher,
+    possa ser mais empático e aprender essas necessidades.
   </div>
 </template>
